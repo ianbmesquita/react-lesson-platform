c3a8daaf8efc3c1538158414e7b6245d8654a76c
ianbmesquita <ianmesquitadb@gmail.com>
2022-06-22 13:10:33 -0300

    feat: Adding GraphQL integration and api access in graphcms.

---
+++ src/lib/apollo.ts
@@ -0,0 +1,5 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+export const client = new ApolloClient({
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pm3krn1wm101xt9uvmdu3t/master',
+    cache: new InMemoryCache()
+})
